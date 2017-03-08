06ea2b11c150387a8977aadee18a172530d82a51
Joshua Wirth <wirth.joshua@gmail.com>
2017-03-08 13:03:11 +0100

    add some tests

---
+++ OpenXmlLibrary.Tests/OpenXmlDocument/SetNewProperty.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using FluentAssertions;
+using OpenXmlDocumentLibrary;
+
+using Xunit;
+
+namespace OpenXmlLibrary.Tests
+{
+ 
+    public class SetNewProperty
+    {
+        OpenXmlDocument CreateOpenXmlDocument()
+        {
+            return new OpenXmlDocument("test.docx");
+        }
+
+        [Fact]
+        public void ShouldNeverThrow_NullReferenceException()
+        {
+            var openXmlDoc = CreateOpenXmlDocument();
+            openXmlDoc.Invoking(x => x.SetNewProperty(null, null, PropertyType.Text)).ShouldNotThrow<NullReferenceException>();
+        }
+
+        [Fact]
+        public void ShouldThrow_WhenPropertyNameIsNull()
+        {
+            var openXmlDoc = CreateOpenXmlDocument();
+            openXmlDoc.Invoking(x => x.SetNewProperty(null, null, PropertyType.Text))
+                .ShouldThrow<ArgumentNullException>()
+                .And.ParamName.Should().Contain("propertyName");
+        }
+
+        [Fact]
+        public void ShouldThrow_WhenPropertyValueIsNull()
+        {
+            var openXmlDoc = CreateOpenXmlDocument();
+            openXmlDoc.Invoking(x => x.SetNewProperty("test", null, PropertyType.Text))
+                .ShouldThrow<ArgumentNullException>()
+                .And.ParamName.Should().Contain("propertyValue");
+        }
+    }
+}
---
+++ OpenXmlLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OpenXmlLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VRSG")]
+[assembly: AssemblyProduct("OpenXmlLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright © VRSG 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5605e60d-a486-49d2-94b7-b3b309bb6c36")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
